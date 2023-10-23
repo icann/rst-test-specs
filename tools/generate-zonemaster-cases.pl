3b24e12da3fde773cf02b67800582316068219aa
Gavin Brown <gavin.brown@icann.org>
2023-10-23 14:59:37 +0200

    make description multiline

--- tools/generate-zonemaster-cases.pl
+++ tools/generate-zonemaster-cases.pl
@@ -109,9 +109,9 @@ foreach my $row ($doc->getElementsByTagName('tr')) {
 
     my $description = sprintf(
         'This test case comes from Zonemaster. ' .
-        'For more information, see ' .
-        'https://github.com/zonemaster/zonemaster/blob/master/docs/public/' .
-        'specifications/tests/%s/%s.md',
+        "For more information, see\n" .
+        '    https://github.com/zonemaster/zonemaster/blob' .
+        '/master/docs/public/specifications/tests/%s/%s.md',
         $plan,
         lc($id)
     );
@@ -138,7 +138,8 @@ foreach my $case (sort grep { $_->{'id'} =~ /^dnssec/ } @cases) {
     printf(
         "%s:\n".
         "  Summary: %s\n".
-        "  Description: %s\n",
+        "  Description: |\n".
+        "    %s\n",
         $case->{'id'},
         $case->{'summary'},
         $case->{'description'},
