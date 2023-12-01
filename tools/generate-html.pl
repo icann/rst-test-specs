ede2a86e116ed32e16e9cf848a2ce01d1e5c3e55
Gavin Brown <gavin.brown@icann.org>
2023-12-01 16:35:55 +0000

    updated

--- tools/generate-html.pl
+++ tools/generate-html.pl
@@ -148,7 +148,7 @@ sub print_preamble_toc_list {
     print $h->summary($h->a({href => '#preamble'}, 'Preamble'));
 
     my $doc = XML::LibXML->load_html(
-        'string'    => $spec->preamble->html,
+        'string'    => $spec->preamble->html(1),
         'encoding'  => 'UTF-8',
     );
 
