f935ecc2e3456588bf79b72cde4fe678c0e7ab8b
Gavin Brown <gavin.brown@icann.org>
2023-12-07 14:30:45 +0000

    updated

--- tools/generate-version.pl
+++ tools/generate-version.pl
@@ -1,30 +1,38 @@
 #!perl
 use strict;
 
+my ($major, $minor, $patch);
+
 #
 # get the most recent tag
 #
 my $tag = `git tag --list | tail -1`;
 chomp($tag);
 
-#
-# remove leading v
-#
-my $last = $tag;
-$last =~ s/^v//;
+if ($tag) {
+    #
+    # remove leading v
+    #
+    my $last = $tag;
+    $last =~ s/^v//;
 
-#
-# split
-#
-my ($major, $minor, $patch) = split(/\./, $last);
+    #
+    # split
+    #
+    ($major, $minor, $patch) = split(/\./, $last);
 
-#
-# get the number of commits since the most recent tag
-#
-$patch = `git rev-list $tag.. --count`;
-chomp($patch);
+    #
+    # get the number of commits since the tag
+    #
+    $patch = `git rev-list $tag.. --count`;
+    chomp($patch);
 
-#
-# generate a new version based on the current release and the number of commits
-#
+} else {
+    $major = 1;
+    $minor = 0;
+    $patch = `git log --oneline | wc -l`;
+
+    chomp($patch);
+}
+
 printf("%u.%u.%u\n", $major, $minor, 1+$patch);
