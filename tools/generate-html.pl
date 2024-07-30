8ff135dd25cf49c549b5af27706287b19581b352
Gavin Brown <gavin.brown@icann.org>
2024-07-30 16:27:04 +0100

    include implementation status for cases, suites and plans

--- tools/generate-html.pl
+++ tools/generate-html.pl
@@ -636,6 +636,17 @@ sub print_plan {
 
     print $h->close(details);
 
+    print $h->open(details);
+
+    print $h->summary($h->h4(sprintf('%u.%u.%u. Implementation status', $section, $i, ++$j)));
+
+    print $h->p(sprintf(
+        'As of the publication of this document, <strong>%u%%</strong> of the test cases in this plan have been implemented in the RST system.',
+        100 * $plan->implemented
+    ));
+
+    print $h->close(details);
+
 =pod
 
     print $h->open(details);
@@ -811,6 +822,17 @@ sub print_suite {
 
     print $h->close(details);
 
+    print $h->open(details);
+
+    print $h->summary($h->h4(sprintf('%u.%u.%u. Implementation status', $section, $i, ++$j)));
+
+    print $h->p(sprintf(
+        'As of the publication of this document, <strong>%u%%</strong> of the test cases in this suite have been implemented in the RST system.',
+        100 * $suite->implemented
+    ));
+
+    print $h->close(details);
+
 =pod
 
 This is disabled until it becomes meaningful.
@@ -894,8 +916,17 @@ sub print_case {
 
     my $j = 0;
 
+=pod
+
+This is no longer really meaningful.
+
     print $h->h4(sprintf('%u.%u.%u. Maturity Level', $section, $i, ++$j));
     print $h->ul($h->li([$h->strong($case->maturity.': '), $mdesc->{$case->maturity}]));
+
+=cut
+
+    print $h->h4(sprintf('%u.%u.%u. Implementation status', $section, $i, ++$j));
+    print $h->ul($h->li(sprintf('This test has <strong>%s</strong> been implemented in the RST system.', $case->implemented ? 'has' : 'has not yet')));
 
     print $h->h4(sprintf('%u.%u.%u. Description', $section, $i, ++$j));
     print $case->description->html(4);
