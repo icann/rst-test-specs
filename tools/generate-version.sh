7b1b982837ae802ce3898f2b8d500324dcf34da0
Gavin Brown <gavin.brown@icann.org>
2026-03-05 16:32:21 +0000

    if the current git branch appears to be a version number, use it.

--- tools/generate-version.sh
+++ tools/generate-version.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 if [ ! -z "$RELEASE" ] ; then
+    echo "Generating version using \$RELEASE environment variable (${RELEASE})" > /dev/stderr
 	echo "\"$RELEASE\"" | tr -d "v"
-
-else
-	echo "\"3.$(date +%Y%j)\""
-
+    exit
 fi
 
+BRANCH="$(git branch | grep '^\*' | cut -b 3- | tr -d v)"
+
+if [[ "$BRANCH" =~ ^[0-9]+(\.[0-9]+)+$ ]] ; then
+    echo "Generating version using the current git branch" > /dev/stderr
+    echo "$BRANCH"
+    exit
+fi
+
+else
+    echo "Generating version using the current date" > /dev/stderr
+	echo "\"3.$(date +%Y%j)\""
+fi
